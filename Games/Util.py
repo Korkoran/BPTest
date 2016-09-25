cd1ab68d3d7d3787687321ecd03dd763eedcddd9
Korkoran <david.koncak@seznam.cz>
2016-09-25 21:00:52 +0200

    Nove Csv

---
+++ Games/Util.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import re
+import collections
+import os.path
+
+current_dir = os.getcwd()
+parent = os.path.split(current_dir)
+
+
+strilecka_session_log = pd.read_csv('%s/CSV/strilecka_session_log.csv' % (parent[0]), header = 0, sep = ';', parse_dates=['time'])
+strilecka_level = pd.read_csv('%s/CSV/strilecka_level.csv' % (parent[0]), header = 0, sep = ';')
+strilecka_level_word = pd.read_csv('%s/CSV/strilecka_level_word.csv' % (parent[0]), header = 0, sep = ';')
+
+roboti_level = pd.read_csv('%s/CSV/roboti_level.csv' % (parent[0]), header = 0, sep = ';')
+roboti_session_log = pd.read_csv('%s/CSV/roboti_session_log.csv' % (parent[0]), header = 0, sep = ';', parse_dates=['time'])
+roboti_shot_log = pd.read_csv('%s/CSV/roboti_shot_log.csv' % (parent[0]), header = 0, sep = ';')
+
+tetris_level = pd.read_csv('%s/CSV/tetris_level.csv' % (parent[0]), header = 0, sep = ';')
+tetris_session_log = pd.read_csv('%s/CSV/tetris_session_log.csv' % (parent[0]), header = 0, sep = ';', parse_dates=['time'])
+tetris_vzor = pd.read_csv('%s/CSV/tetris_vzor.csv' % (parent[0]), header = 0, sep = ';')
+tetris_word = pd.read_csv('%s/CSV/tetris_word.csv' % (parent[0]), header = 0, sep = ';')
+
+print strilecka_level_word
